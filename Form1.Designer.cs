a16885cb09c5bcbfaed020d96029ebc63654a35d
Jake Hurd <legendbreakerjake@gmail.com>
2022-04-25 02:08:55 +0100

    Latest Current Bugsplatter Installer
    On the fly!

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,39 +28,123 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(93, 213);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(153, 15);
+            this.label1.Size = new System.Drawing.Size(113, 15);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Downloading latest version.";
+            this.label1.Text = "Awaiting user input.";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(12, 27);
+            this.progressBar1.Location = new System.Drawing.Point(93, 231);
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(235, 23);
+            this.progressBar1.Size = new System.Drawing.Size(265, 23);
             this.progressBar1.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(288, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(70, 70);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Bugsplatter Beta";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(37, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(235, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "This is an installer / updater for Bugsplatter.";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 49);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(253, 15);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Simply place this updater in a folder, run it and";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 65);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(242, 15);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "download and unpack the latest Bugsplatter!";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 231);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Begin";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 92);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(309, 15);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Don\'t worry about clogged files, this installer cleans itself.";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(259, 61);
+            this.ClientSize = new System.Drawing.Size(370, 266);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Bugsplatter Updater";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -70,5 +154,12 @@
 
         private Label label1;
         private ProgressBar progressBar1;
+        private PictureBox pictureBox1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Button button1;
+        private Label label6;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -14,35 +14,47 @@ namespace UpdaterProcessTest
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            label1.Text = "Removing old zip if present.";
-            File.Delete(@".\RetroTest_latest.zip");
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //    label1.Text = "Removing old zip if present."; // shhhhhh not yet a full package ;)
+            //    File.Delete(@".\Bugsplatter.zip"); // That'll be used later.
+            label1.Text = "Deleting old version if present.";
+            File.Delete(@".\Bugsplatter.exe");
             label1.Text = "Downloading latest version.";
             WebClient client = new WebClient();
             client.DownloadProgressChanged += new DownloadProgressChangedEventHandler(client_DownloadProgressChanged);
             client.DownloadFileCompleted += new System.ComponentModel.AsyncCompletedEventHandler(client_DownloadFileCompleted);
-            client.DownloadFileAsync(new Uri("https://github.com/retr0gr4d3/AutoUpdaterTest/releases/download/Stable/RetroTest_latest.zip"), @".\update.zip");
+            client.DownloadFileAsync(new Uri("https://github.com/retr0gr4d3/QuickFix/releases/download/working/Bugsplatter_latest.exe"), @".\Bugsplatter.exe");
         }
 
         void client_DownloadFileCompleted(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
         {
-            label1.Text = "File downloaded. Deleting old version.";
-            File.Delete(@".\\RetroTest.png");
-            label1.Text = "File deleted. Unpacking...";
-            string zipPath = @".\update.zip";
-            string extractPath = @".\";
-            try
-            {
-                ZipFile.ExtractToDirectory(zipPath, extractPath);
-            }
-            catch
-            {
-                MessageBox.Show("Error! Something went wrong. Opening Bugsplatter.");
-                Process.Start("Bugsplatter_Beta.exe");
-            }
-            label1.Text = "Deleting update.zip...";
-            File.Delete(@".\update.zip");
+            label1.Text = "File(s) downloaded.";
+        //    File.Delete(@".\Bugsplatter.exe");
+        //    label1.Text = "File deleted. Unpacking...";
+        //    string zipPath = @".\update.zip";
+        //    string extractPath = @".\";
+        //    try
+        //    {
+        //        ZipFile.ExtractToDirectory(zipPath, extractPath);
+        //    }
+        //    catch
+        //    {
+        //        MessageBox.Show("Error! Something went wrong. Opening Bugsplatter.");
+        //        Process.Start("Bugsplatter_Beta.exe");
+        //    }
+        //    label1.Text = "Deleting update.zip...";
+        //    File.Delete(@".\update.zip");
             label1.Text = "Update complete!";
-            Process.Start(@".\Bugsplatter_Beta.exe");
+            Process.Start(@".\Bugsplatter.exe");
             this.Close();
         }
 
@@ -52,9 +64,5 @@ namespace UpdaterProcessTest
             progressBar1.Value = (int)e.BytesReceived / 100;
         }
 
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
     }
 }
